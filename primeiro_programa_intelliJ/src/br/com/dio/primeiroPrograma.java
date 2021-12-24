22659bc229426c50724ec372c9813ad22195fc95
Rony <rony.msalu@gmail.com>
2021-12-24 10:44:16 -0300

    criando class livro

--- primeiro_programa_intelliJ/src/br/com/dio/primeiroPrograma.java
+++ primeiro_programa_intelliJ/src/br/com/dio/primeiroPrograma.java
@@ -8,9 +8,45 @@ public class primeiroPrograma {
         System.out.println(gato);
 
 
-       /* int a=5;
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
+        /* int a=5;
         int b=3;
         System.out.println("Hello World! " + (a + b));*/
     }
 
+}
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
